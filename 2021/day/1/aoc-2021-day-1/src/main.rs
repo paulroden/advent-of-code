957bf226be6f261dda8d94453320a47b836c772c
Paul Roden <git@paulroden.net>
2021-12-01 11:04:42 +0000

    read input file - greedy strings at once

--- 2021/day/1/aoc-2021-day-1/src/main.rs
+++ 2021/day/1/aoc-2021-day-1/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let _text = read_input().expect("could not read input file!");
+}
+
+
+fn read_input() -> Result<Vec<String>, io::Error> {
+    let file = File::open("../input")?;
+    let buffer = BufReader::new(file);
+
+    let mut lines = Vec::new();
+
+    for line in buffer.lines() {
+        if let Some(line) = &line.ok() {
+            lines.push(line.clone());
+
+            println!("{:?}", &line);
+        }
+    }
+    Ok(lines)
 }

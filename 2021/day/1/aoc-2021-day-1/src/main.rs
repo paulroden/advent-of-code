c7fcc4b065d85a1a6d697c7fd7347f6c682c46b9
Paul Roden <git@paulroden.net>
2021-12-01 12:43:23 +0000

    naively read lines to i32s

--- 2021/day/1/aoc-2021-day-1/src/main.rs
+++ 2021/day/1/aoc-2021-day-1/src/main.rs
@@ -1,22 +1,26 @@
 use std::fs::File;
+use std::path::Path;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() {
-    let _text = read_input().expect("could not read input file!");
+    let input_file_path = Path::new("../input");
+    let text = read_input_lines(input_file_path).expect("could not read input file!");
+
+    println!("{:?}", text);
 }
 
 
-fn read_input() -> Result<Vec<String>, io::Error> {
-    let file = File::open("../input")?;
+fn read_input_lines(path: &Path) -> Result<Vec<i32>, io::Error> {
+    let file = File::open(path)?;
     let buffer = BufReader::new(file);
 
     let mut lines = Vec::new();
 
     for line in buffer.lines() {
         if let Some(line) = &line.ok() {
-            lines.push(line.clone());
-
-            println!("{:?}", &line);
+            if let Ok(number) = line.parse::<i32>() {
+                lines.push(number)
+            }
         }
     }
     Ok(lines)
